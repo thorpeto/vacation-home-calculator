5eed5d7e4e0a9e56fd1679a517e41ed6c30407b5
thorpeto <tpetersson@web.de>
2025-09-10 15:27:09 +0000

    Version 0.3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = 'vacation-home-calculator';
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   images: {
     unoptimized: true
   }
